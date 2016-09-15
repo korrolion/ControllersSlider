7c606ac89a7c6c638c36865c470a2d271564b403
Igor Korolev <ikorolev@wamba.com>
2016-09-15 12:47:13 +0300

    Initial Commit

---
+++ AnketaSlider/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AnketaSlider
+//
+//  Created by Королев Игорь on 15.09.16.
+//  Copyright © 2016 Igor Korolev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
